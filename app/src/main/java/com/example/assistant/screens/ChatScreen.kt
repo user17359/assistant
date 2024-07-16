76d5bd9b0f3653bd6ecda51d2496cdd5e341ac34
Ryszard Błażej <sapermysza@gmail.com>
2024-07-16 14:00:38 +0200

    Scrolling and test emojis

--- app/src/main/java/com/example/assistant/screens/ChatScreen.kt
+++ app/src/main/java/com/example/assistant/screens/ChatScreen.kt
@@ -9,35 +9,53 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.assistant.data.Message
+import androidx.compose.foundation.lazy.items
 import com.example.assistant.data.mockData
 import com.example.assistant.elements.ChatBubble
 import com.example.assistant.viewModel.ChatViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatScreen(chatViewModel: ChatViewModel){
     
     var last: Boolean? = null
     
-    Scaffold { innerPadding ->
+    Scaffold(
+        topBar = {TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text("Conversation")
+            }
+        )}
+    ) { innerPadding ->
         Box(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
         ){
-            Column (
+            LazyColumn (
                 modifier = Modifier
                     .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(5.dp)
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                reverseLayout = true
             ) {
-                for(message: Message in mockData){
+                items(mockData){ message ->
                     if(last != null && last != message.user){
-                        Spacer(modifier = Modifier.height(5.dp))
+                        Spacer(modifier = Modifier.height(15.dp))
                     }
                     if(message.user) {
                         Row(
