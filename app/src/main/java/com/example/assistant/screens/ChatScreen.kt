fcd726cda83ea2caa29e0e2d28c7685d20761145
Ryszard Błażej <sapermysza@gmail.com>
2024-07-16 14:50:24 +0200

    Added bottom bar for writing messages

--- app/src/main/java/com/example/assistant/screens/ChatScreen.kt
+++ app/src/main/java/com/example/assistant/screens/ChatScreen.kt
@@ -1,5 +1,6 @@
 package com.example.assistant.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,6 +22,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.assistant.R
 import com.example.assistant.data.mockData
 import com.example.assistant.elements.ChatBubble
 import com.example.assistant.viewModel.ChatViewModel
@@ -30,7 +43,8 @@ import com.example.assistant.viewModel.ChatViewModel
 fun ChatScreen(chatViewModel: ChatViewModel){
     
     var last: Boolean? = null
-    
+    var text by remember { mutableStateOf("") }
+
     Scaffold(
         topBar = {TopAppBar(
             colors = TopAppBarDefaults.topAppBarColors(
@@ -47,47 +61,82 @@ fun ChatScreen(chatViewModel: ChatViewModel){
                 .padding(innerPadding)
                 .fillMaxSize()
         ){
-            LazyColumn (
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(5.dp),
-                reverseLayout = true
-            ) {
-                items(mockData){ message ->
-                    if(last != null && last != message.user){
-                        Spacer(modifier = Modifier.height(15.dp))
-                    }
-                    if(message.user) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(end = 15.dp),
-                            horizontalArrangement = Arrangement.End
-                        ) {
-                            ChatBubble(
-                                message.content,
-                                color = Color.Blue,
-                                textColor = Color.White
-                            )
+            Column {
+                LazyColumn(
+                    modifier = Modifier
+                        .weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    reverseLayout = true
+                ) {
+                    items(mockData) { message ->
+                        if (last != null && last != message.user) {
+                            Spacer(modifier = Modifier.height(15.dp))
                         }
-                    }
-                    else {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 15.dp),
-                            horizontalArrangement = Arrangement.Start
-                        ) {
-                            ChatBubble(
-                                message.content,
-                                color = Color.LightGray,
-                                textColor = Color.Black
-                            )
+                        if (message.user) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(end = 15.dp),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                ChatBubble(
+                                    message.content,
+                                    color = Color.Blue,
+                                    textColor = Color.White
+                                )
+                            }
+                        } else {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 15.dp),
+                                horizontalArrangement = Arrangement.Start
+                            ) {
+                                ChatBubble(
+                                    message.content,
+                                    color = Color.LightGray,
+                                    textColor = Color.Black
+                                )
+                            }
                         }
+                        last = message.user
                     }
-                    last = message.user
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 15.dp)
+                        .height(80.dp)
+                        .background(MaterialTheme.colorScheme.primaryContainer),
+                    verticalAlignment = Alignment.CenterVertically,
+                ){
+                    TextField(
+                        modifier = Modifier
+                            .padding(start = 15.dp, end = 15.dp)
+                            .fillMaxWidth()
+                            .weight(1f),
+                        value = text,
+                        onValueChange = {text = it}
+                    )
+                    IconButton(
+                        modifier = Modifier.padding(end = 15.dp),
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_send_24),
+                            contentDescription = "Send button",
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    }
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun previewChatScreen()
+{
+    ChatScreen(viewModel())
 }
